0d8a5c92c94d82a1865b4406cb1e17a56ea63c1a
Temanlyob <kanaiyaj542@gmail.com>
2026-07-28 14:40:08 +0530

    Create results.js

---
+++ results.js
@@ -0,0 +1,63 @@
+// Buttons
+const homeBtn = document.getElementById("homeBtn");
+const calendarBtn = document.getElementById("calendarBtn");
+
+// Result Elements
+const resultIcon = document.getElementById("resultIcon");
+const resultTitle = document.getElementById("resultTitle");
+const scoreValue = document.getElementById("scoreValue");
+
+// Demo Data
+// Baad me ye values localStorage se aayengi.
+let isCorrect = true;
+
+let currentStreak = 3;
+let bestStreak = 8;
+let winRate = 82;
+
+// Update Stats
+document.getElementById("currentStreak").innerHTML =
+currentStreak + " Days";
+
+document.getElementById("bestStreak").innerHTML =
+bestStreak + " Days";
+
+document.getElementById("winRate").innerHTML =
+winRate + "%";
+
+// Correct / Wrong Theme
+if(isCorrect){
+
+    resultIcon.innerHTML = "🏆";
+
+    resultTitle.innerHTML = "Today's Result";
+
+    scoreValue.innerHTML = "+10";
+
+}else{
+
+    resultIcon.innerHTML = "❌";
+
+    resultTitle.innerHTML = "Better Luck Tomorrow";
+
+    scoreValue.innerHTML = "0";
+
+    scoreValue.style.color = "#ef4444";
+
+    resultIcon.style.background = "#ffecec";
+
+}
+
+// Home
+homeBtn.onclick = function(){
+
+    window.location.href = "index.html";
+
+}
+
+// Calendar
+calendarBtn.onclick = function(){
+
+    alert("Calendar page coming soon.");
+
+}
