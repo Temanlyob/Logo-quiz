a2f7f9299fa74615dac8994fcac818454ac2842c
Temanlyob <kanaiyaj542@gmail.com>
2026-07-28 23:49:22 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,48 @@
+import {
+  auth,
+  googleProvider,
+  createUserDocument,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  onAuthStateChanged
+} from "./auth.js";
 
+// Already logged in?
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "home.html";
+  }
+});
+
+// Email Login
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "home.html";
+  } catch (err) {
+    alert(err.message);
+  }
+});
+
+// Google Login
+const googleBtn = document.getElementById("googleLogin");
+
+googleBtn.addEventListener("click", async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+
+    await createUserDocument(result.user);
+
+    window.location.href = "home.html";
+
+  } catch (err) {
+    alert(err.message);
+  }
+});
