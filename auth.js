b7dadfa534a3388eeca200ca7eab9591e5ae1b4b
Temanlyob <kanaiyaj542@gmail.com>
2026-07-29 18:13:30 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -19,25 +19,44 @@ import {
 // Save user to Firestore if first login
 async function createUserDocument(user) {
 
-  const ref = doc(db, "users", user.uid);
+  try {
 
-  const snap = await getDoc(ref);
+    alert("createUserDocument Started");
 
-  if (!snap.exists()) {
+    const ref = doc(db, "users", user.uid);
 
-    await setDoc(ref, {
-      uid: user.uid,
-      username: user.displayName || "User",
-      email: user.email,
-      photoURL: user.photoURL || "",
-      totalScore: 0,
-      puzzlesPlayed: 0,
-      gamesWon: 0,
-      gamesLost: 0,
-      currentStreak: 0,
-      bestStreak: 0,
-      createdAt: serverTimestamp()
-    });
+    const snap = await getDoc(ref);
+
+    alert("Document Already Exists: " + snap.exists());
+
+    if (!snap.exists()) {
+
+      alert("Creating Firestore Document...");
+
+      await setDoc(ref, {
+        uid: user.uid,
+        username: user.displayName || "User",
+        email: user.email,
+        photoURL: user.photoURL || "",
+        totalScore: 0,
+        puzzlesPlayed: 0,
+        gamesWon: 0,
+        gamesLost: 0,
+        currentStreak: 0,
+        bestStreak: 0,
+        createdAt: serverTimestamp()
+      });
+
+      alert("Firestore Document Created");
+
+    }
+
+  } catch (err) {
+
+    console.error(err);
+
+    alert("ERROR CODE:\n" + err.code);
+    alert("ERROR MESSAGE:\n" + err.message);
 
   }
 
