c0a56a3a1f623d2632971daf252d3ddf1dc5c995
Temanlyob <kanaiyaj542@gmail.com>
2026-08-07 15:18:18 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,33 @@
+const savedTheme = localStorage.getItem("theme") || "system";
 
+function applyTheme() {
+
+  let theme = savedTheme;
+
+  if (theme === "system") {
+
+    theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+
+  }
+
+  document.body.classList.remove("light", "dark");
+  document.body.classList.add(theme);
+
+}
+
+applyTheme();
+
+window.matchMedia("(prefers-color-scheme: dark)")
+.addEventListener("change", () => {
+
+  if ((localStorage.getItem("theme") || "system") === "system") {
+
+    applyTheme();
+
+  }
+
+});
+
+export { applyTheme };
