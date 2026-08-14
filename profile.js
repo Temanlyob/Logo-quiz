34b92c6e9f8a8a24a4cb6190e8c02db6b8fbefc7
Temanlyob <kanaiyaj542@gmail.com>
2026-08-14 18:52:55 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,4 +1,14 @@
-import { auth, db } from "./firebase.js";
+import {
+  auth,
+  db,
+  storage
+} from "./firebase.js";
+
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
 import {
 onAuthStateChanged,
